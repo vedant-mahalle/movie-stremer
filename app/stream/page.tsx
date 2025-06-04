b02ecb254c9f7c17b3140b96e28fff4d96397ea7
Vedant Mahalle <vedantmahalle39@gmail.com>
2025-06-04 17:27:35 +0530

    Streaming now 2

--- app/stream/page.tsx
+++ app/stream/page.tsx
@@ -1,4 +1,17 @@
-"use client";
+// "use client";
+
+// import { Suspense } from "react";
+// import StreamVideoPlayer from "../../components/StreamVideoPlayer";
+
+// export default function StreamPage() {
+//   return (
+//     <main className="flex flex-col items-center justify-center p-4">
+//       <Suspense fallback={<div>Loading video...</div>}>
+//         <StreamVideoPlayer />
+//       </Suspense>
+//     </main>
+//   );
+// }
 
 import { Suspense } from "react";
 import StreamVideoPlayer from "../../components/StreamVideoPlayer";
